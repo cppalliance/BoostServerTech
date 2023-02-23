f41c17683d726b405bf48130847f79ac4c09022a
Vinnie Falco <vinnie.falco@gmail.com>
2023-02-23 11:48:48 -0800

    tidy up build script and bring in deps

--- source/main.cpp
+++ source/main.cpp
@@ -1,11 +1,37 @@
-#include <iostream>
-
-#include "lib.hpp"
+#include <boost/buffers.hpp>
+#include <boost/http_proto.hpp>
+#include <boost/http_io.hpp>
+#include <boost/asio.hpp>
 
 auto main() -> int
 {
-  auto const lib = library {};
-  auto const message = "Hello from " + lib.name + "!";
-  std::cout << message << '\n';
-  return 0;
+    // buffers
+    {
+        char temp[16];
+        boost::buffers::circular_buffer cb(
+            { temp, sizeof(temp) });
+        cb.prepare(5);
+        cb.commit(3);
+        cb.data();
+    }
+
+    // http_proto
+    {
+        boost::http_proto::context ctx;
+    }
+
+    // http_io
+    {
+        boost::http_proto::context ctx;
+        boost::http_proto::serializer sr(4096);
+        boost::asio::io_context ioc;
+        boost::asio::ip::tcp::socket sock(ioc);
+        boost::http_io::async_write(
+            sock, sr, [](
+            boost::system::error_code,
+            std::size_t)
+            {
+            });
+
+    }
 }
